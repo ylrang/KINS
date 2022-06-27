49cc1c151b694a33281fbef21b52e102e078a812
ljhyun715 <ljhyun715@ncsquare.co.kr>
2022-06-27 17:57:39 +0900

    06/27

--- static/js/pages/job-list.init.js
+++ static/js/pages/job-list.init.js
@@ -1,3 +1,4 @@
-var singleCategories,checks,singleLocation=new Choices("#choices-single-location"),
-singleCategorie=document.getElementById("choices-single-categories");singleCategorie&&(singleCategories=new Choices("#choices-single-categories")),
-check=document.getElementById("choices-single-checks");check&&(checks=new Choices("#choices-single-checks"));
+// var singleCategories,checks,singleLocation=new Choices("#choices-single-location"),
+// singleCategorie=document.getElementById("choices-single-categories");singleCategorie&&(singleCategories=new Choices("#choices-single-categories")),
+// check=document.getElementById("choices-single-checks");check&&(checks=new Choices("#choices-single-checks"));
+var singleCategories=new Choices("#choices-single-categories")
