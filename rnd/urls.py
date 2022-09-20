50b5302b3545826c188298355d325abdddb71fa6
ljhyun715 <ljhyun715@ncsquare.co.kr>
2022-09-20 17:52:38 +0900

    09/20

--- rnd/urls.py
+++ rnd/urls.py
@@ -7,7 +7,12 @@ app_name='rnd'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
+
     path('case', views.case, name='case'),
+    path('KAERI_case', views.KAERI, name='KAERI_case'),
+    path('KORAD_case', views.KORAD, name='KORAD_case'),
+    path('create_case', views.create_case, name='create_case'),
+
     path('institute', views.institute, name='institute'),
 
 ]
--- rnd/views.py
+++ rnd/views.py
@@ -10,6 +10,17 @@ def case(request):
     return render(request, "rnd/case.html")
 
 
+def KAERI(request):
+    return render(request, "rnd/KAERI_case.html")
+
+def KORAD(request):
+    return render(request, "rnd/KORAD_case.html")
+
+
+def create_case(request):
+    return render(request, "rnd/create_case.html")
+
+
 def about(request):
     return render(request, "rnd/about.html")
 
