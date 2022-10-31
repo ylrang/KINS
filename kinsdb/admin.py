98fc27121ec932b67988a943432b136c3a2ffa13
ljhyun715 <ljhyun715@ncsquare.co.kr>
2022-10-31 15:32:10 +0900

    version1.0

--- kinsdb/admin.py
+++ kinsdb/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 from .models import Tag, Docs, Document, Site, Keyword, SWFactor
-# from import_export.admin import ImportExportModelAdmin
-# from .resource import DocsResource
+from import_export.admin import ImportExportModelAdmin
+from .resource import DocsResource
 
 admin.site.register(Tag)
-admin.site.register(Docs)
+# admin.site.register(Docs)
 admin.site.register(Document)
 admin.site.register(Keyword)
 admin.site.register(Site)
 admin.site.register(SWFactor)
 
-# class DocsAdmin(ImportExportModelAdmin):
-#     resource_class = DocsResource
-#     list_display = ('title', 'serial_num', 'index_num', 'index_title')
-#
-#
-# admin.site.register(Docs, DocsAdmin)
+class DocsAdmin(ImportExportModelAdmin):
+    resource_class = DocsResource
+    list_display = ('title', 'index_num')
+
+
+admin.site.register(Docs, DocsAdmin)
