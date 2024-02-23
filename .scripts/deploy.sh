e1008d25f8124f1ee5a075c3e7311aef508d02d1
ylrang <yl5649@ncsquare.co.kr>
2024-02-23 15:12:21 +0900

    cicd workflow

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "Deployment started ..."               
-
-# Pull the latest version of the app
-echo "Copying New changes...."
-git pull https://${{ secrets.ACCESS_TOKEN }}@github.com/ylrang/KINS.git main
-echo "New changes copied to server !"
-
 # Activate Virtual Env
-source source /home/devncnc/project/venv/bin/activate             # activate your virtual environment. change zvenv with your venv name
+source /home/devncnc/project/venv/bin/activate
 echo "Virtual env 'venv' Activated !"
 
-echo "Clearing Cache..."
-python manage.py clean_pyc
-python manage.py clear_cache
+#echo "Clearing Cache..."
+#python manage.py clean_pyc
+#python manage.py clear_cache
 
 echo "Installing Dependencies..."
 pip install -r requirements.txt --no-input
